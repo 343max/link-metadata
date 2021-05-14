529b0bd9b98b21f774e64dd8af0c7fe85b2927d2
Max von Webel <max.von.webel@finleap.com>
2021-05-14 14:27:58 +0200

    updated name and metadata

---
+++ ios/LinkMetadataModule.m
@@ -0,0 +1,11 @@
+//
+//  LinkMetadataModule.m
+//  LinkMetadataModule
+//
+//  Copyright © 2021 Max von Webel. All rights reserved.
+//
+
+#import <React/RCTBridgeModule.h>
+
+@interface RCT_EXTERN_MODULE(LinkMetadataModule, NSObject)
+@end
