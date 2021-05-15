372e09a3f51232b41e79c1824718bb8162f4d057
Max von Webel <max.von.webel@finleap.com>
2021-05-15 20:13:32 +0200

    rendering the preview image

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -5,6 +5,7 @@ import {fetchURLPreview, LinkMetadata} from 'react-native-link-metadata'
 const LinkPreview: FC = () => {
   const url = "https://de.wikipedia.org/wiki/Vulcanodon"
   const [linkMetadata, setLinkMetadata] = useState<LinkMetadata | undefined>()
+  const [size, setSize] = useState({width: 1, height: 1})
 
   useEffect(() => {
     const f = async () => {
@@ -14,22 +15,32 @@ const LinkPreview: FC = () => {
     f()
   }, [])
 
+  useEffect(() => {
+    if (linkMetadata?.imageURL) {
+    Image.getSize(linkMetadata.imageURL, (width, height) => {
+      setSize({width, height})
+    })}
+  }, [linkMetadata])
+
   if (linkMetadata === undefined) {
     return <Text>Loading...</Text>
   } else {
+    const style = {width: 300, height: 300 / size.width * size.height}
     return (<View>
       <Text>{linkMetadata.originalUrl}</Text>
       <Text>{linkMetadata.url}</Text>
       <Text>{linkMetadata.title}</Text>
       {linkMetadata.imageURL !== undefined ?
-      <Image source={{uri: linkMetadata.imageURL}} style={{width: "100%"}} /> : <></>}
+      <Image source={{uri: linkMetadata.imageURL}} style={style} /> : <></>}
     </View>)
   }
 
 }
 
-const App = () => {
-  return <SafeAreaView><LinkPreview /></SafeAreaView>
-}
+const App = () => (
+  <SafeAreaView style={{margin: 20}}>
+    <LinkPreview />
+  </SafeAreaView>
+)
 
 export default App
